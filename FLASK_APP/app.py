f554bea848e2513123931e20f307bb14ee1e7ce4
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-11-06 16:19:20 +0530

    importing multiple file , controler stracture

--- FLASK_APP/app.py
+++ FLASK_APP/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 # import controller.user_controller
 # import controller.product_controller
 
-from controller import user_controller, product_controller
+from controller import user_controller, product_controller, product_categories_controller
+# from controller import *
 
 @app.route("/")
 def welcome():
@@ -14,3 +16,5 @@ def welcome():
 @app.route("/home")
 def home():
     return "This is my home"
+
+app.run(debug=True, port=5001)
---
+++ FLASK_APP/controller/product_categories_controller.py
@@ -0,0 +1,6 @@
+from app import app
+@app.route("/pcat/addnew")
+def pcat_addnew():
+    return "This is pcat addnew operation"
+
+#product_categories_controller
